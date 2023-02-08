d3e2236d5184e32ac8f6276c12a32ffde74abcaa
Valerio Luzzi <valluzzi@gmail.com>
2023-02-08 17:42:37 +0100

    some fix

--- docker-build-m2.bat
+++ docker-build-m2.bat
@@ -1,5 +1,5 @@
 echo @off
-docker buildx build --file Dockerfile-m2 --platform linux/amd64,linux/arm64 --push -t m2/gdal .
+docker build --file Dockerfile-m2 -t m2/gdal .
 git add .
 git commit -m "some fix"
 git push 
